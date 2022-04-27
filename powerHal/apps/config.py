9e2516e89ebdf086ef7b40083a683618d8d50ae7
MaverickFR <maverick.perrollaz@pm.me>
2022-04-27 15:37:15 +0200

    wip

--- powerHal/apps/config.py
+++ powerHal/apps/config.py
@@ -8,13 +8,20 @@ from decouple import config
 
 class Config(object):
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
+    DEBUG = True
 
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
-    # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    # MySQL database
+    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+        config('DB_ENGINE', default='postgresql'),
+        config('DB_USERNAME', default='appseed'),
+        config('DB_PASS', default='pass'),
+        config('DB_HOST', default='localhost'),
+        config('DB_PORT', default=5432),
+        config('DB_NAME', default='appseed-flask')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
