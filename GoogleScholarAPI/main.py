72da2eee5e84ad0ce789d6826ca85aace1c66a37
Forray_Gabriel <gabriel.forray@gmail.com>
2022-04-27 13:01:42 +0200

    Added data base integration to scholar API

--- GoogleScholarAPI/main.py
+++ GoogleScholarAPI/main.py
@@ -1,3 +1,16 @@
-from model import Publication
-from API import Publication_Getter
+from model.Publication import Publication
+from API.Publication_Getter import Publication_Getter
+from config.DB import DB
 
+dataBase = DB()
+
+author = Publication_Getter('Sebastien Monnet')
+
+author.setDataBase(dataBase)
+
+publi = author.get_publications(4)
+
+for i in publi:
+    print('\n\n\n')
+    for j in i :
+        print(j)
--- GoogleScholarAPI/model/Publication.py
+++ GoogleScholarAPI/model/Publication.py
@@ -1,6 +1,10 @@
 class Publication():
 
-    def __init__(self,titre, pub_date, description,author,affiliation, pdf_lien):
+    def __init__(self,database, id_scholar):
+        self.database = database
+        self.id_scholar = id_scholar
+
+    def set_infos(self, titre, pub_date, description,author,affiliation, pdf_lien):
         self.titre = titre
         self.pub_date = pub_date
         self.description = description
@@ -10,3 +14,13 @@ class Publication():
 
     def get_infos(self):
         return self.titre, self.pub_date, self.description, self.author, self.affiliation, self.pdf_lien
+
+    # Permet d'ajouter l'objet database à l'objet Publication
+    def setDataBase(self, database):
+        self.database = database
+
+    # Permet de vérifier si l'objet concept existe dans la base de données et l'ajouter dans le cas échéant
+    def checkIfExistsOrInsert(self):
+        return self.database.checkIfExistsOrInsert(self, fieldsComparable = ['id_scholar'])
+
+
