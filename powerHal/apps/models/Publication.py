a3f9b24f16b9bd7e00eb24fcf6fbf362ee3cc095
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2022-05-01 22:36:47 +0200

    powerHall 0.2 - add exemple of model

--- powerHal/apps/models/Publication.py
+++ powerHal/apps/models/Publication.py
@@ -1,11 +1,6 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from apps import db
 
-class Publications(db.Model):
+class Publication(db.Model):
     __tablename__ = 'publication'
     id_publication = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(500), unique=True)
--- powerHal/apps/publication/routes.py
+++ powerHal/apps/publication/routes.py
@@ -5,12 +5,12 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
-from apps.publication.models import Publications
+from apps.models import Publication
 
 
 @blueprint.route('/publication')
 @login_required
 def publication():
-    return render_template('publications/publications.html', publication = Publications.query.all(), segment='index')
+    return render_template('publications/publications.html', publication = Publication.query.all(), segment='index')
 
 
