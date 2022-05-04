e949d7186cd511450ef8b9e9ff600e698d411dc8
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2022-05-04 09:07:24 +0200

    scaperAPI 0.3 - add biblio information in publication

--- scraperAPI/model/entities/Publication.py
+++ scraperAPI/model/entities/Publication.py
@@ -1,13 +1,17 @@
 class Publication():
 
     # Permet de créer un objet de type publication
-    def __init__(self, doi, title, display_name, type, publication_year, publication_date, updated_date, created_date, id_source, citation_count):
+    def __init__(self, doi, title, display_name, type, publication_year, publication_date, biblio_volume, biblio_first_page, biblio_last_page, biblio_issue, updated_date, created_date, id_source, citation_count):
         self.id_doi = doi
         self.title = title
         self.display_name = display_name
         self.type_publication = type
         self.publication_year = publication_year
         self.publication_date = publication_date
+        self.biblio_volume = biblio_volume 
+        self.biblio_first_page = biblio_first_page
+        self.biblio_last_page = biblio_last_page
+        self.biblio_issue = biblio_issue
         self.updated_date = updated_date
         self.created_date = created_date
         self.id_source = id_source
