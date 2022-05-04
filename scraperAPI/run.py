79d9a1f53e7ae5a0105ecbccf00218c385add61c
Forray_Gabriel <gabriel.forray@gmail.com>
2022-05-04 10:27:11 +0200

    Add infos to publications

--- scraperAPI/run.py
+++ scraperAPI/run.py
@@ -1,8 +1,11 @@
+from scraperAPI.visualization.PowerCloud import PowerCloud
+from scraperAPI.visualization.PowerGraph import PowerGraph
 from specific.openAlex.openAlex import openAlex
 from specific.openCitation.openCitation import openCitation
 from specific.googleScholar.googleScholar import googleScholar
 from specific.hal.hal import hal
 from config.DB import DB
+
 
 class Run():
 
@@ -20,13 +23,17 @@ class Run():
     # openAlex(dataBase,research, id_connected_user)
 
     # Gestion du pont API Google Scholar
-    # googleScholar(dataBase,"Sebastien Monnet",2)
+    googleScholar(dataBase,research,5)
 
     # Gestion du pont avec Open Citation
     # openCitation(dataBase,research, id_connected_user)
 
     # Gestion du point avec HAL
-    hal(dataBase,research, id_connected_user, id_author_as_user)
+    # hal(dataBase,research, id_connected_user, id_author_as_user)
+
+    # Creation du graph
+    PowerGraph(dataBase, id_connected_user)
+
 
 
 
