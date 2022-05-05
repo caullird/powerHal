a63e8bea76b18d0fcc244afed8e3ff6dd57fad79
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2022-05-05 22:04:01 +0200

    openCitation 1.1 - author adaptation

--- scraperAPI/specific/openCitation/openCitation.py
+++ scraperAPI/specific/openCitation/openCitation.py
@@ -1,6 +1,4 @@
 # Importation des classes pour la configuration globale au projet
-from config.DB import DB
-from config.ResearchInitializer import ResearchInitializer
 from config.ResearchSource import ResearchSource
 
 # Importation des configurations propre à l'API
@@ -10,20 +8,16 @@ from specific.openCitation.API.CitationAPI import CitationAPI
 
 class openCitation():
 
-    def __init__(self,dataBase,research, id_connected_user):
+    def __init__(self,dataBase,research):
         self.dataBase = dataBase
-        self.research = research
-        self.id_connected_user = id_connected_user
+        self.id_connected_user = research['id_connected_user']
         self.API = openCitationAPI()
         self.run()
 
     def run(self):
-       
-        initialize = ResearchInitializer(self.research)
         
         ## Permet de récupérer les informations relative à la source (id_source)
         sourceID = ResearchSource("OpenCitation", self.dataBase).getMySQLID()
 
         ## Récupération des informations relative à l'API
-
         CitationAPI(self.API, self.dataBase, self.id_connected_user, sourceID)
