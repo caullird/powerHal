bf0fec3b7e92976fd201529de3b50dbd1a5d1fef
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2022-05-06 13:14:16 +0200

    powerHal 0.4.4 - replace SVG

--- powerHal/apps/section/profil/forms.py
+++ powerHal/apps/section/profil/forms.py
@@ -6,11 +6,11 @@ from wtforms.validators import Email, DataRequired
 
 # login and registration
 
-
 class ProfilAuthorForm(FlaskForm):
     author_name = StringField('Prénom',
                          id='author_name',
                          validators=[DataRequired()])
+
     author_forname = StringField('Nom de famille',
                              id='author_forname',
                              validators=[DataRequired()])
