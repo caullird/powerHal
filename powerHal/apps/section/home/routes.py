90cf3974b2a243f4116fb07a2f367f6d0fc8cb61
Forray_Gabriel <gabriel.forray@gmail.com>
2022-05-12 22:04:09 +0200

    Changed dashboard

--- powerHal/apps/section/home/routes.py
+++ powerHal/apps/section/home/routes.py
@@ -28,26 +28,19 @@ from flask_login import (
 @blueprint.route('/index')
 @login_required
 def index():
+
     author = Author.query.filter_by(id_author=current_user.id_author,created_by=current_user.id).first()
 
     countPublication = AuthorPublication.query.filter_by(id_author=current_user.id_author,created_by=current_user.id).count()
 
     countCitation = 0
-    
+
     for publication in Publication.query.filter_by(created_by=current_user.id).all():
         authorPublications = AuthorPublication.query.filter_by(id_publication=publication.id_publication,created_by=current_user.id,id_author=current_user.id_author).all()
         if authorPublications :
             countCitation += int(publication.citation_count)
 
-
-    print(current_user.id)
     return render_template('home/dashboard.html', author = author, countPublication = countPublication, countCitation = countCitation, segment='index')
-
-
-@blueprint.route('/graph.png')
-def graph_png():
-    img = requests.get("http://localhost:8000/graph/" + str(current_user.id))
-    return Response(img, mimetype='image/png')
 
 @blueprint.route('/cloud.png')
 def cloud_png():
